d77bb7151708244bd68f9e333e20d92dd54da74e
adamking314 <king.adam314@gmail.com>
2025-05-20 19:03:55 -0700

    reverting what chat gpt told me

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,25 +1,26 @@
 import consumer from "./consumer"
 
 import $ from 'jquery';
+
 import {
   indexChats,
 } from "../lib/requests.js";
 
-
-
 consumer.subscriptions.create("ChatChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log("Front-end: connected to the chat channel!");
+    console.log("Connected to the chat channel!");
   },
+
   disconnected() {
     // Called when the subscription has been terminated by the server
-    console.log("Front-end: disconnected from the chat channel!");
+    console.log("Disconnected from the chat channel!");
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log("Front-end: received data from the chat channel: ", data);
+    console.log(data);
+
     indexChats(function (response) {
       var htmlString = response.chats.map(function(chat) {
         return `
@@ -36,6 +37,7 @@ consumer.subscriptions.create("ChatChannel", {
           </li>
         `;
       });
+
       $("#chats").html(htmlString);
     });
   }
