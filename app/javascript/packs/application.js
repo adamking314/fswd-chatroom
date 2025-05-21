64d51ec174dc5d0a28d6f1745c5e8a09bd48e418
adamking314 <king.adam314@gmail.com>
2025-05-20 20:12:50 -0700

    removing what chat gpt said

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../stylesheets/application.tailwind.css";
 
 // import 'bootstrap/dist/js/bootstrap.bundle'
 
